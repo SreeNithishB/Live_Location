9088fa7730fcdcd512d4714a5bf01674cd20f6c3
Sree Nithish B <sreenithishbofficial@gmail.com>
2021-03-01 22:20:57 +0530

    Added Folium Map

--- location/views.py
+++ location/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import LocationModelForm
+import folium
 
 # Create your views here.
 
@@ -7,13 +8,34 @@ def location_views(request):
 
     form = LocationModelForm(request.POST or None)
 
+    # initial values
+    l_lat = '33.8688'
+    l_lon = '151.2093'
+    m = folium.Map(width=800, height=500, location=[l_lat, l_lon], zoom_start=8)
+    folium.Marker([l_lat, l_lon], tooltip="click here for more", popup="Hey!",
+                    icon=folium.Icon(color='purple')).add_to(m)
+
     if form.is_valid():
         instance = form.save(commit=False)
         instance.latitude = form.cleaned_data.get('latitude')
         instance.longitude = form.cleaned_data.get('longitude')
+
+        l_lat = instance.latitude
+        l_lon = instance.longitude
+
         form.save()
 
+        # initial folium map
+        m = folium.Map(width=800, height=500, location=[l_lat, l_lon], zoom_start=8)
+
+        # location Marker
+        folium.Marker([l_lat, l_lon], tooltip="click here for more", popup="Hey!",
+                        icon=folium.Icon(color='purple')).add_to(m)
+
+    m = m._repr_html_()
+
     context = {
-        'form': form
+        'form': form,
+        'map': m
     }
     return render(request, 'location/location.html', context)
